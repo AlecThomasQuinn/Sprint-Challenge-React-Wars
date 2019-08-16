47195d541186515ab83fea3dacef783e5969d285
unknown <alec.thomas.quinn@gmail.com>
2019-08-16 11:47:41 -0500

    axios getting, persons are mapping, cards are turning into people. I AM A GOD

--- starwars/src/components/PersonCard.js
+++ starwars/src/components/PersonCard.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const PersonCard = () => {
+const PersonCard = (props) => {
     return(
-        <div>I am more than just a div I am a person I swear.</div>
+        <div>I am more than just a div I am a person I swear. See I have ID: {props.name}</div>
     );
 };
 
--- starwars/src/components/PersonGrid.js
+++ starwars/src/components/PersonGrid.js
@@ -4,9 +4,26 @@ import axios from 'axios';
 import { default as Card} from './PersonCard.js';
 
 const PersonGrid = () => {
+    const [personArr, setPersonArr] = useState([])
+
+    useEffect(() => {
+        axios
+            .get(`https://swapi.co/api/people`)
+            .then(response => {
+                console.log('data getted from SWAPI:', response.data.results);
+
+                setPersonArr(response.data.results);
+            })
+            .catch(console.log('aint got nothin brah'))
+    }, []);
 
     return(
-        <Card />
+
+        <>
+            {personArr.map(person => 
+                <Card name = {person.name}/>)}
+            <Card />
+        </> 
     );
 
 };
