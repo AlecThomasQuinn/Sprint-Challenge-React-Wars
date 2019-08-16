9bccdef4bc1d88723e73340c6b339d8de04c67f9
unknown <alec.thomas.quinn@gmail.com>
2019-08-16 12:07:15 -0500

    i am not a god i am just a man... nothing is rendering anymore :(

--- starwars/src/components/PersonCard.js
+++ starwars/src/components/PersonCard.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Card, Image } from 'semantic-ui-react';
 
 const PersonCard = (props) => {
     return(
+        <Card> {/* just now realzing that I perhaps should not have chnaged the
+        names of my components into these more generic terms when I imported them. 
+        I mostly did the renames though just because I could, to practice doing it,
+        so I will keep them for the project as reference. In the future though I will
+        take more care with using more descriptive/specific naming conventions*/}
+            <Image src='' wrapped ui={false} />
+            <Card.Content>
+                <Card.Header>{props.name}</Card.Header>
+                <Card.Meta>
+                    <span className='date'>Born: {props.birth_year}</span>
+                </Card.Meta>
+            </Card.Content>
+
         <div>I am more than just a div I am a person I swear. See I have ID: {props.name}</div>
+        </Card>
     );
 };
 
